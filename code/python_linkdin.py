3e80673f6088125314b8cc91ce8b89295d642aa2
Rushi-Bhavsar <rushi.bhavsar.57@gmail.com>
2022-10-07 01:48:17 +0530

    Added code to get connection basic information from search page only.

--- code/python_linkdin.py
+++ code/python_linkdin.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 from parse_yaml import cred_data
 
@@ -17,8 +18,8 @@ class PythonLinkedin:
 
     def account_login(self):
         """
-        Code to login into linkedin home.
-        :return: None
+        Code to log in into LinkedIn home.
+        @return: None
         """
         self.driver.get(self.__cred_data.login)
         username = self.driver.find_element(by=By.XPATH, value='//*[@id="session_key"]')
@@ -32,7 +33,7 @@ class PythonLinkedin:
     def account_logout(self):
         """
         Logout of the account.
-        :return: None
+        @return: None
         """
         self.driver.execute_script("window.open('');")
         sleep(2)
@@ -43,8 +44,8 @@ class PythonLinkedin:
     def get_search_url(self, role):
         """
         Search by the keyword and return the search url.
-        :param role: Job Designation.
-        :return: Search result URL.
+        @param role: Job Designation.
+        @return: Search result URL.
         """
         search_bar = self.driver.find_element(by=By.XPATH, value='//*[@id="global-nav-typeahead"]/input')
         search_bar.send_keys(role)
@@ -53,12 +54,36 @@ class PythonLinkedin:
         search_by_people = self.driver.find_element(by=By.XPATH, value='//button[text()="People"]')
         search_by_people.click()
         sleep(5)
+        ul_element = self.driver.find_elements(by=By.XPATH, value='//*[@id="main"]/div/div/div[1]/ul/li')
+        for item in ul_element:
+            try:
+                image = item.find_element(by=By.TAG_NAME, value='img')
+            except NoSuchElementException:
+                name = item.find_element(by=By.CLASS_NAME, value='visually-hidden')
+                print(f"Name: {name.text}")
+                print("Image is not present.")
+                image = None
+            if image:
+                image_link = image.get_attribute('src')
+                name = image.get_attribute('alt')
+                print(f"Name: {name}")
+                print(f'Image Link: {image_link}')
+            link = item.find_element(by=By.XPATH, value='.//div/div/div[1]/div/a').get_attribute('href')
+            print(f'Link: {link.split("?")[0]}')
+            skill = item.find_element(by=By.XPATH, value='.//div/div/div[2]/div[1]/div[2]/div[1]')
+            print(f"Skill: {skill.text}")
+            location = item.find_element(by=By.XPATH, value='.//div/div/div[2]/div[1]/div[2]/div[2]')
+            print(f'Location: {location.text}')
+            info = item.find_element(by=By.XPATH, value='.//div/div/div[2]/div[2]/p')
+            print(f"Info: {info.text}")
+            print('-'*50)
+
         return self.driver.current_url
 
     def close_driver(self):
         """
         Quit Chrome Browser.
-        :return: None
+        @return: None
         """
         self.driver.quit()
 
@@ -66,7 +91,7 @@ class PythonLinkedin:
 def driver_code():
     pl = PythonLinkedin()
     pl.account_login()
-    search_url = pl.get_search_url("senior python developer")
+    search_url = pl.get_search_url("Django Developer")
     print(f"Search URL: {search_url}")
     sleep(5)
 
